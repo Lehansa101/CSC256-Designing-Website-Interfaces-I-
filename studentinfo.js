a042974650e18e214bbc7862e9f7cd0af469c72d
Lehansa Fernando <k.lehansafernando@gmail.com>
2026-05-31 19:29:01 -0700

    Add files via upload

---
+++ studentinfo.js
@@ -0,0 +1,14 @@
+document.getElementById("name").innerHTML =
+    "<strong>Name:</strong> Lehansa Fernando";
+
+document.getElementById("major").innerHTML =
+    "<strong>Major:</strong> Computer Science, Robotics Engineering, Artificial Intelligence, and Data Science";
+
+document.getElementById("email").innerHTML =
+    "<strong>UAT Email:</strong> kfernando86343@uat.edu";
+
+document.getElementById("graduation").innerHTML =
+    "<strong>Expected Graduation:</strong> May 2028";
+
+document.getElementById("extra").innerHTML =
+    "I have experience with C++, Python, React Native, and web development. One project I built was a space-themed assignment tracker.";
